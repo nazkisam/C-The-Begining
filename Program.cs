7ecb6dd26aed079f920b510c8634c8f2efa0234d
Sameem <nsam603@gmail.com>
2025-05-10 16:04:28 +0530

    type Casting

--- Program.cs
+++ Program.cs
@@ -12,6 +12,64 @@ class Program{
   static void Main(string[] args){
 
 
+
+
+
+
+ // Type casting
+// two types of type casting , implicit casting:- char to int to long to float to double, char to others gives  the  Asci value
+ int x = 3;
+ float y = x;
+ Console.WriteLine(y);
+
+
+
+ //explicit casting : When the type casting id manual and is done by the coder
+int p  = (int) 3.4;
+Console.WriteLine(p);
+
+double n =  3;
+Console.WriteLine(n);
+ 
+// casting using the methods.
+float var = Convert.ToInt32(3.44);
+Console.WriteLine(var);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //if else and data types 
+  
+
+  bool isGreat = false;
+  Console.WriteLine(isGreat);
+
+  if (isGreat == true){
+
+Console.Write("true");
+  }
+  else{
+    Console.WriteLine("false");
+  }
+
+
+
+
+
+
+
+
     int harry  = 30;  //int takes 4 bytes
     string inp = Console.ReadLine() ?? string.Empty; //char takes 2 bytes bool takes one bit, long takes 8 bytes,doule takes 8 bytes upt to 15  decimal digits precision 
     Console.WriteLine(inp);
